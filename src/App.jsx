b36be8e8ecdb3c3fbe2592ce26a66536a6fe1566
Mohd Adnan <86228023+Mohdadnan2320@users.noreply.github.com>
2024-09-04 16:07:45 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Footer from "./components/Footer";
 const App = () => {
   return (
     <div className="overflow-hidden ">
-      <div className=" bg-cover  bg-no-repeat w-full h-screen bg-[url('https://images.unsplash.com/photo-1670594457103-129309beb706?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] md:h-[100vh] lg:h-screen ">
+      <div className=" bg-cover  bg-no-repeat w-full h-screen bg-[url('https://images.unsplash.com/photo-1670594457103-129309beb706?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] md:h-[100vh] lg:h-max ">
         <Navbar />
         <Hero />
       </div>
